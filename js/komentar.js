011eb02c84c73966443d3118d667ac64326624ca
andhim <anditoinfo@gmail.com>
2025-05-26 21:37:52 +0700

    perbaikan fungsi komentar

--- js/komentar.js
+++ js/komentar.js
@@ -1,18 +1,12 @@
+// fungsi komnetar cusdis 
 document.addEventListener("DOMContentLoaded", function () {
-  const thread = document.getElementById('cusdis_thread');
+  const thread = document.getElementById("cusdis_thread");
 
-  // Pastikan elemen dan library Cusdis tersedia
-  if (thread && window.Cusdis) {
-    const pageUrl = window.location.href;
-    const pageId = pageUrl; // Bisa diganti dengan slug jika ingin lebih pendek
-    const pageTitle = document.title;
-
-    window.Cusdis.renderTo('cusdis_thread', {
-      host: 'https://cusdis.com',
-      appId: '70af2b20-fc8f-4162-9c16-53d2f449da4a',
-      pageId: pageId,
-      pageUrl: pageUrl,
-      pageTitle: pageTitle
-    });
+  if (thread) {
+    const path = window.location.pathname.replace(/^\/|\/$/g, "") || "beranda";
+    thread.dataset.pageId = path;
+    thread.dataset.pageUrl = window.location.href;
+    thread.dataset.pageTitle = document.title;
   }
 });
+
